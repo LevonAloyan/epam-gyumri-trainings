711ac9b8f6518d99477dcde1d012bce2369aab0a
HaykDavtyan <Hayk9710@gmail.com>
2021-10-29 18:42:46 +0400

    Homework_2_Hayk_Davtyan Figures

--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -3,7 +3,19 @@ package figureprinter;
 public class FigurePrinter {
 
     public static void main(String[] args) {
-        FigurePainter.drawLeftUpTriangle(5);
-
+        FigurePainter.drawLeftUpTriangle(4);
+        System.out.println(" ");
+        FigurePainter.drawRightUpTriangle(5);
+        System.out.println(" ");
+        FigurePainter.drawLeftBottomTriangle(4);
+        System.out.println(" ");
+        FigurePainter.drawRightBottomTriangle(4);
+        System.out.println(" ");
+        FigurePainter.drawRhombus(5);
+        System.out.println(" ");
+        FigurePainter.drawIsoscelesTriangle(5);
+        System.out.println(" ");
+        FigurePainter.drawBottomIsoscelesTriangle(5);
+        System.out.println(" ");
     }
 }
