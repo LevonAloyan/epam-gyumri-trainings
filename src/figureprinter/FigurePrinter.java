5ecb58dd86b94d4ae585dbf3c5072e84e636f83f
Levon_Aloyan <>
2021-10-29 17:56:43 +0400

    Revert merge

--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -4,18 +4,6 @@ public class FigurePrinter {
 
     public static void main(String[] args) {
         FigurePainter.drawLeftUpTriangle(4);
-        System.out.println("_______________________________");
-        FigurePainter.drawRightUpTriangle(5);
-        System.out.println("_______________________________");
-        FigurePainter.drawLeftBottomTriangle(4);
-        System.out.println("_______________________________");
-        FigurePainter.drawRightBottomTriangle(4);
-        System.out.println("_______________________________");
-        FigurePainter.drawRhombus(4);
-        System.out.println("_______________________________");
-        FigurePainter.drawIsoscelesTriangle(5);
-        System.out.println("_______________________________");
-        FigurePainter.drawBottomIsoscelesTriangle(5);
 
     }
 }
