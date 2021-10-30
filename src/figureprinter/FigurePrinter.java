868a7a50516d3a3be3c9b6465b45c880e2a448cd
User <tsarukyan.edgar94@gmail.com>
2021-10-31 00:19:53 +0400

    My second homework in Epam Armenia

--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -4,6 +4,11 @@ public class FigurePrinter {
 
     public static void main(String[] args) {
         FigurePainter.drawLeftUpTriangle(4);
-
+        FigurePainter.drawRightUpTriangle(5);
+        FigurePainter.drawLeftBottomTriangle(4);
+        FigurePainter.drawRightBottomTriangle(4);
+        FigurePainter.drawRhombus(5);
+        FigurePainter.drawIsoscelesTriangle(8);
+        FigurePainter.drawBottomIsoscelesTriangle(11);
     }
 }
