e92b72b28c8b5828ffc821ffb38349796005cff9
Armen9601 <77sd977>
2021-10-31 01:37:28 +0400

    homework 1

--- src/figureprinter/FigurePainter.java
+++ src/figureprinter/FigurePainter.java
@@ -15,7 +15,13 @@ public class FigurePainter {
      */
     public static void drawLeftUpTriangle(int length){
         // TODO : complete the method
-
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("*");
+        }
+        System.out.println();
     }
 
     /**
@@ -31,7 +37,16 @@ public class FigurePainter {
      */
     public static void drawRightUpTriangle(int length){
         // TODO : complete the method
-
+        for (int i = length; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = length + 1; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
     /**
      * Draws the triangle by specified length number
@@ -47,6 +62,13 @@ public class FigurePainter {
      */
     public static void drawLeftBottomTriangle(int length){
         // TODO : complete the method
+        for (int i = length; i > 0; i--) {
+            for (int j = i - 1; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println("*");
+        }
+        System.out.println();
     }
 
     /**
@@ -63,6 +85,16 @@ public class FigurePainter {
      */
     public static void drawRightBottomTriangle(int length){
         // TODO : complete the method
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < length; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
     /**
      * Draws the rhombus by specified length number
@@ -80,6 +112,25 @@ public class FigurePainter {
      */
     public static void drawRhombus(int length) {
         // TODO : complete the method
+        for (int i = length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = length; j > i; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < length; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
     /**
      * lenght = 5
@@ -92,7 +143,30 @@ public class FigurePainter {
      * @param length specified length of triangle will be drawn
      */
     public static void drawIsoscelesTriangle(int length){
+        if (length % 2 == 0){
+            for (int i = 1; i <= length; i += 2) {
+                for (int j = length; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" *");
+                }
 
+                System.out.println();
+            }
+        }else {
+            for (int i = 1; i <= length + 1; i += 2) {
+                for (int j = length; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" *");
+                }
+
+                System.out.println();
+            }
+        }
+        System.out.println();
     }
 
     /**
@@ -106,7 +180,29 @@ public class FigurePainter {
      * @param length specified length of triangle will be drawn
      */
     public static void drawBottomIsoscelesTriangle(int length){
+        if (length % 2 == 0){
+            for (int i = length - 1; i >= 1; i -= 2) {
+                for (int j = i; j < length; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = i; j > 0; j--) {
+                    System.out.print(" *");
+                }
 
+                System.out.println();
+            }
+        }else {
+            for (int i = length; i >= 1; i -= 2) {
+                for (int j = i; j < length; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = i; j > 0; j--) {
+                    System.out.print(" *");
+                }
+
+                System.out.println();
+            }
+        }
     }
 
 }
