2f74cb9aec2a4f91805d52c0cc68b0375b72d599
Lilit Manukyan <lilitmanukyan888@gmail.com>
2021-10-30 21:00:43 +0400

    Done homework

--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -6,7 +6,7 @@ public class FigurePrinter {
         FigurePainter.drawLeftUpTriangle(4);
         FigurePainter.drawRightUpTriangle(5);
         FigurePainter.drawLeftBottomTriangle(4);
-        FigurePainter.drawRightBottomTriangle(4); 
+        FigurePainter.drawRightBottomTriangle(4);
         FigurePainter.drawRhombus(5);
         FigurePainter.drawIsoscelesTriangle(5);
         FigurePainter.drawBottomIsoscelesTriangle(5);
