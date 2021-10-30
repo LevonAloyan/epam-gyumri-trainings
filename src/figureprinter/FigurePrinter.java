aa3d22a26569addd8ff82c0d064068d3ffe5873a
HeghineKhachatryan <Heghine9696@gmail.com>
2021-10-30 20:25:04 +0400

    Done homework 2

--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -3,12 +3,12 @@ package figureprinter;
 public class FigurePrinter {
 
     public static void main(String[] args) {
-      //  FigurePainter.drawLeftUpTriangle(5);
-       // FigurePainter.drawRightUpTriangle(5);
-      //  FigurePainter.drawLeftBottomTriangle(5);
-      //  FigurePainter.drawRightBottomTriangle(5);
+        FigurePainter.drawLeftUpTriangle(5);
+         FigurePainter.drawRightUpTriangle(5);
+        FigurePainter.drawLeftBottomTriangle(5);
+        FigurePainter.drawRightBottomTriangle(5);
         FigurePainter.drawRhombus(5);
-       // FigurePainter.drawIsoscelesTriangle(5);
-      //  FigurePainter.drawBottomIsoscelesTriangle(5);
+        FigurePainter.drawIsoscelesTriangle(5);
+       FigurePainter.drawBottomIsoscelesTriangle(5);
     }
 }
