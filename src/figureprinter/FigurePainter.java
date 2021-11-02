331e23ab538eaa4c8db4fe364968bf74c94a942c
Hayk Matevosyan <hayk.matevosyan@yahoo.com>
2021-11-02 22:48:19 +0400

    Done homework 2

--- src/figureprinter/FigurePainter.java
+++ src/figureprinter/FigurePainter.java
@@ -13,57 +13,96 @@ public class FigurePainter {
      *
      * @param length  specified length of triangle will be drawn
      */
-    public static void drawLeftUpTriangle(int length){
-        // TODO : complete the method
+        public static void drawLeftUpTriangle(int length) {
+            // TODO : complete the method
 
+            for (int i = 1; i <= length; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+
+        }
+
+        /**
+         * Draws the triangle by specified length number
+         * for example when n = 5 , it must be such as
+         * drawn below :
+         *             *
+         *           * *
+         *         * * *
+         *       * * * *
+         *     * * * * *
+         * @param length  specified length of triangle will be drawn
+         */
+        public static void drawRightUpTriangle(int length) {
+            // TODO : complete the method
+
+            for (int i = 1; i <= length; i++) {
+                for (int j = 0; j < length; j++) {
+                    if (j < length - i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+
+        }
+
+        /**
+         * Draws the triangle by specified length number
+         * for example when n = 4 , it must be such as
+         * drawn below :
+         *
+         *       * * * *
+         *       * * *
+         *       * *
+         *       *
+         *
+         * @param length  specified length of triangle will be drawn
+         */
+    public static void drawLeftBottomTriangle(int length) {
+        for (int i = length; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        // TODO : complete the method
     }
 
-    /**
-     * Draws the triangle by specified length number
-     * for example when n = 5 , it must be such as
-     * drawn below :
-     *             *
-     *           * *
-     *         * * *
-     *       * * * *
-     *     * * * * *
-     * @param length  specified length of triangle will be drawn
-     */
-    public static void drawRightUpTriangle(int length){
-        // TODO : complete the method
 
-    }
+
     /**
      * Draws the triangle by specified length number
      * for example when n = 4 , it must be such as
      * drawn below :
      *
      *       * * * *
-     *       * * *
-     *       * *
-     *       *
-     *
-     * @param length  specified length of triangle will be drawn
-     */
-    public static void drawLeftBottomTriangle(int length){
-        // TODO : complete the method
-    }
-
-    /**
-     * Draws the triangle by specified length number
-     * for example when n = 4 , it must be such as
-     * drawn below :
-     *
-     *       * * * *
      *         * * *
      *           * *
      *             *
      *
      * @param length  specified length of triangle will be drawn
      */
-    public static void drawRightBottomTriangle(int length){
+    public static void drawRightBottomTriangle(int length) {
         // TODO : complete the method
+        for (int i = length; i > 0; i--) {
+            for (int j = 0; j < length; j++) {
+                if (j < length - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
     }
+
     /**
      * Draws the rhombus by specified length number
      * for example when n = 5 , it must be such as
--- src/figureprinter/FigurePrinter.java
+++ src/figureprinter/FigurePrinter.java
@@ -4,6 +4,9 @@ public class FigurePrinter {
 
     public static void main(String[] args) {
         FigurePainter.drawLeftUpTriangle(4);
+        FigurePainter.drawRightBottomTriangle(4);
+        FigurePainter.drawLeftBottomTriangle(4);
+        FigurePainter.drawRightUpTriangle(4);
 
     }
 }
