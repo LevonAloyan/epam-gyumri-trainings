c9a17318efdf731e251175b994affd01cf7f68ba
Davit012 <jandavjan5252@mail.ru>
2021-11-04 14:51:57 +0400

    add arrayUtil methods

--- src/arrays/ArrayUtilTest.java
+++ src/arrays/ArrayUtilTest.java
@@ -1,8 +1,16 @@
 package arrays;
 
 public class ArrayUtilTest {
-
+    private static final int[] array = new int[]{2, 3, 5};
     public static void main(String[] args) {
-
+        ArrayUtil.createArray(5);
+        ArrayUtil.create2dArray(5,5);
+        ArrayUtil.addFirst(array, 1);
+        System.out.println(ArrayUtil.contains(array, 1));
+        System.out.println(ArrayUtil.indexOf(array, 3));
+        ArrayUtil.remove(array, 2);
+        System.out.println(ArrayUtil.sum(array));
+        System.out.println(ArrayUtil.getMax(array));
+        System.out.println(ArrayUtil.getAvg(array));
     }
 }
