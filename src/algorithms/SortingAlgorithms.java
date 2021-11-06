b278d4f8ee6968acdf7fcbed2d03cd08f6c19f19
ART <creativeavetisyan@gmail.com>
2021-11-07 01:05:05 +0400

    Artash

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -3,45 +3,133 @@ package algorithms;
 public class SortingAlgorithms {
 
     public static void main(String[] args) {
-        int [] array = new int[]{2,1,8,3,5,5,-26};
-        sortByBubbleSorting(array);
+        int[] array = new int[]{2, 1, 8, 3, 5, 5, -26};
+//        sortByBubbleSorting(array);
+//        sortBySelectionSorting(array);
+//        sortByInsertionSorting(array);
+        sortByQuickSorting(array);
         print(array);
     }
 
     public static void sortByBubbleSorting(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-i-1; j++) {
-                if (array[j] > array[j+1]) {
-                    swap(array, j+1, j);
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j + 1, j);
                 }
             }
         }
     }
 
-    public static void sortBySelectionSorting(int[] array){
+    public static void sortBySelectionSorting(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(array, min, i);
+            }
+        }
+    }
+
+    public static void sortByInsertionSorting(int[] array) {
+        int num = array.length;
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > value) {
+                swap(array, j + 1, j);
+                j--;
+            }
+        }
+    }
+
+    public static void sortByMergeSorting(int[] array) {
+        mergeSort(array, 0, array.length - 1);
 
     }
 
-    public static void sortByInsertionSorting(int[] array){
+    static void mergeSort(int[] array, int low, int high) {
+        if (low < high) {
+            int mid = low + (high - low) / 2;
+            mergeSort(array, low, mid);
+            mergeSort(array, mid + 1, high);
+            merge(array, low, mid, high);
+        }
+    }
+
+    static void merge(int[] array, int low, int mid, int high) {
+        int[] leftArray = new int[mid - low + 1];
+        int[] rightArray = new int[high - mid];
+
+        for (int i = 0; i < leftArray.length; i++)
+            leftArray[i] = array[low + i];
+        for (int i = 0; i < rightArray.length; i++)
+            rightArray[i] = array[mid + i + 1];
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        for (int i = low; i < high + 1; i++) {
+            if (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+                if (leftArray[leftIndex] < rightArray[rightIndex]) {
+                    array[i] = leftArray[leftIndex];
+                    leftIndex++;
+                } else {
+                    array[i] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+            } else if (leftIndex < leftArray.length) {
+                array[i] = leftArray[leftIndex];
+                leftIndex++;
+            } else if (rightIndex < rightArray.length) {
+                array[i] = rightArray[rightIndex];
+                rightIndex++;
+            }
+        }
 
     }
 
-    public static void sortByMergeSorting(int[] array){
+    public static void sortByQuickSorting(int[] array) {
+
+        quickSort(array, 0, array.length - 1);
 
     }
 
-    public static void sortByQuickSorting(int[] array){
+    public static void quickSort(int[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
 
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
     }
 
-    private static void swap(int [] array, int firstIndex, int lastIndex) {
+    static int partition(int[] array, int start, int end) {
+        int tmp = array[end];
+        int i = (start - 1);
+        for (int j = start; j < end; j++) {
+            if (array[j] <= tmp) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, end);
+        return i + 1;
+    }
+
+    private static void swap(int[] array, int firstIndex, int lastIndex) {
         int tempValue = array[firstIndex];
         array[firstIndex] = array[lastIndex];
         array[lastIndex] = tempValue;
     }
-    private static void print (int [] array){
-        for(int element : array){
-            System.out.print(element+",");
+
+    private static void print(int[] array) {
+        for (int element : array) {
+            System.out.print(element + ",");
         }
     }
 }
