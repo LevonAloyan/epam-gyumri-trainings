7ddfd358ece87e64320d4353197e88631d799019
Armine Zakaryan <ArmineZak>
2021-11-06 20:12:16 +0400

    Done homework 4.

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -3,45 +3,117 @@ package algorithms;
 public class SortingAlgorithms {
 
     public static void main(String[] args) {
-        int [] array = new int[]{2,1,8,3,5,5,-26};
-        sortByBubbleSorting(array);
+        int[] array = new int[]{2, 1, 8, 3, 5, 8, 444, 65, 5, -26};
+//        sortByBubbleSorting(array);
+//        sortByInsertionSorting(array);
+//        sortBySelectionSorting(array);
+//        sortByMergeSorting(array);
+        sortByQuickSorting(array);
         print(array);
     }
 
     public static void sortByBubbleSorting(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-i-1; j++) {
-                if (array[j] > array[j+1]) {
-                    swap(array, j+1, j);
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(array, j + 1, j);
                 }
             }
         }
     }
 
-    public static void sortBySelectionSorting(int[] array){
-
+    public static void sortBySelectionSorting(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                    swap(array, minIndex, i);
+                }
+            }
+        }
     }
 
-    public static void sortByInsertionSorting(int[] array){
-
+    public static void sortByInsertionSorting(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+            while (j > 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j] = current;
+        }
     }
 
-    public static void sortByMergeSorting(int[] array){
+    public static void sortByMergeSorting(int[] array) {
+        if (array.length < 2)
+            return;
 
+        int middle = array.length / 2;
+
+        int[] left = new int[middle];
+        for (int i = 0; i < middle; i++)
+            left[i] = array[i];
+        int[] right = new int[array.length - middle];
+        for (int i = middle; i < array.length; i++)
+            right[i - middle] = array[i];
+
+        sortByMergeSorting(left);
+        sortByMergeSorting(right);
+        merge(left, right, array);
     }
 
-    public static void sortByQuickSorting(int[] array){
+    private static void merge(int[] left, int[] right, int[] result) {
+        int i = 0, j = 0, k = 0;
 
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                result[k++] = left[i++];
+            else
+                result[k++] = right[j++];
+        }
+        while (i < left.length)
+            result[k++] = left[i++];
+        while (j < right.length)
+            result[k++] = right[j++];
     }
 
-    private static void swap(int [] array, int firstIndex, int lastIndex) {
+
+    public static void sortByQuickSorting(int[] array) {
+        sort(array, 0, array.length - 1);
+    }
+
+    private static void sort(int[] array, int start, int end) {
+        if (start >= end)
+            return;
+
+        int boundary = partition(array, start, end);
+
+        sort(array, start, boundary - 1);
+        sort(array, boundary + 1, end);
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int boundary = start - 1;
+        for (int i = start; i <= end; i++)
+            if (array[i] <= pivot)
+                swap(array, i, ++boundary);
+
+        return boundary;
+    }
+
+
+    private static void swap(int[] array, int firstIndex, int lastIndex) {
         int tempValue = array[firstIndex];
         array[firstIndex] = array[lastIndex];
         array[lastIndex] = tempValue;
     }
-    private static void print (int [] array){
-        for(int element : array){
-            System.out.print(element+",");
+
+    private static void print(int[] array) {
+        for (int element : array) {
+            System.out.print(element + ",");
         }
     }
 }
--- src/arrays/ArrayUtilTest.java
+++ src/arrays/ArrayUtilTest.java
@@ -3,6 +3,17 @@ package arrays;
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
-
+        int[] array = {1, 3, 5};
+        int[] array1 = {1, 3, 5, 12, 8, -5, -125};
+//        ArrayUtil.createArray(5);
+//        ArrayUtil.create2dArray(4, 10);
+//        System.out.println(ArrayUtil.getMax(array));
+//        ArrayUtil.getMin(array);
+//        System.out.println(ArrayUtil.getAvg(array));
+//        System.out.println(ArrayUtil.sum(array));
+//        System.out.println(ArrayUtil.contains(array, 5));
+//        ArrayUtil.remove(array, 2);
+//        System.out.println(ArrayUtil.indexOf(array1, 5));
+        System.out.println(ArrayUtil.addFirst(array, 100));
     }
 }
