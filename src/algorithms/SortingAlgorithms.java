53fd40e55bda263ffb54a2e23248a7b50e9f7098
karen <kshahvaladyan97@gmail.com>
2021-11-07 22:01:32 +0400

    Done Homework 4

--- src/algorithms/SortingAlgorithms.java
+++ src/algorithms/SortingAlgorithms.java
@@ -4,8 +4,16 @@ public class SortingAlgorithms {
 
     public static void main(String[] args) {
         int [] array = new int[]{2,1,8,3,5,5,-26};
-        sortByBubbleSorting(array);
-        print(array);
+//        sortByBubbleSorting(array);
+//        print(array);
+//        sortBySelectionSorting(array);
+//        print(array);
+//        sortByInsertionSorting(array);
+//        print(array);
+//        sortByMergeSorting(array);
+//        print(array);
+//        sortByQuickSorting(array,0,array.length-1);
+//        print(array);
     }
 
     public static void sortByBubbleSorting(int[] array) {
@@ -18,20 +26,59 @@ public class SortingAlgorithms {
         }
     }
 
-    public static void sortBySelectionSorting(int[] array){
-
+    public static void sortBySelectionSorting(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minValue = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < minValue) {
+                    minValue = array[j];
+                    swap(array, j, i);
+                }
+            }
+        }
     }
 
     public static void sortByInsertionSorting(int[] array){
-
+        for (int i = 1; i < array.length; i++) {
+            int currentValue = array[i];
+            int j = 0;
+            for (j = i - 1; j >= 0 && array[j] > currentValue; j--) {
+                array[j + 1] = array[j];
+            }
+            array[j + 1] = currentValue;
+        }
     }
 
     public static void sortByMergeSorting(int[] array){
 
     }
 
-    public static void sortByQuickSorting(int[] array){
 
+    public static void sortByQuickSorting(int[] array,int begin,int end){
+        if(begin>=end){
+            return;
+        }
+        int p = partition(array, begin, end);
+        if(begin < p-1){
+            sortByQuickSorting(array, begin, p - 1);
+        }
+        if(p<end){
+            sortByQuickSorting(array, p + 1, end);
+        }
+
+
+    }
+    private static int partition (int array[], int begin, int end) {
+        int pivot = array[end];
+        int i = (begin-1);
+        for (int b = begin; b < end; b++){
+            if (array[b] <= pivot){
+                i++;
+                swap(array,i,b);
+            }
+        }
+        swap(array,i+1,end);
+        return i+1;
     }
 
     private static void swap(int [] array, int firstIndex, int lastIndex) {
