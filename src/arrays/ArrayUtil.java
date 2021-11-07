ac4e08e7227a0909fe7e65e88c2ae38377816c35
SamvelStepanyan92 <93207327+SamvelStepanyan92@users.noreply.github.com>
2021-11-08 00:09:25 +0400

    Done_Homework4_Samvel_Stepanyan

--- src/arrays/ArrayUtil.java
+++ src/arrays/ArrayUtil.java
@@ -11,34 +11,14 @@ public class ArrayUtil {
      * Create an array with the given size, fill it with the different values and print it.
      */
     public static void createArray(int size) {
-        System.out.println("createArray");
 
-        int[] arr = new int[size];
-        for (int i : arr) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-        System.out.println("_______________________");
     }
 
     /**
      * Create two dimensional array with the given sizes, fill it with different values and print it.
      */
     public static void create2dArray(int x, int y) {
-        System.out.println("create2dArray");
-
-        int[][] arr1 = new int[x][y];
-        for (int i = 0; i < y; i++) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-        for (int j = 0; j < x; j++) {
-            System.out.print(j + ", ");
-        }
-        System.out.println();
-        System.out.println("_______________________");
     }
-
 
     /**
      * Add the given value at the beginning of the given array
@@ -46,132 +26,70 @@ public class ArrayUtil {
      * @return new array.
      */
     public static int[] addFirst(int[] array, int valueToAdd) {
-        System.out.println("addFirst");
 
-        int[] arr = new int[array.length];
-        arr[0] = valueToAdd;
-        System.arraycopy(arr, 0, array, 0, 1);
-        return array;
+        return null;
     }
-
 
     /**
      * Implement a method that will check if the given value contains in the array
+     *
+     * @return true if the array contains the given value, otherwise return false.
      */
-    public static boolean contains(int[] array1, int value) {
-        System.out.println("contains");
+    public static boolean contains(int[] array, int value) {
 
-        for (int i : array1) {
-            if (value == array1[i]) {
-                return true;
-            }
-        }
         return false;
     }
 
     /**
-     * @param array2 An array to search in.
+     * @param array An array to search in.
      * @param value The value to search for.
      * @return the first index of value in the array, if array does not contain given element return -1.
      */
-    public static int indexOf(int[] array2, int value) {
-        System.out.println("indexOf");
+    public static int indexOf(int[] array, int value) {
 
-        for (int i : array2) {
-            if (value == array2[i]) {
-                return value;
-            }
-        }
-        return -1;
+        return 0;
     }
 
     /**
      * Remove the element by the given index.
      * Print new array
      */
-    public static void remove(int[] array3, int index) {
-        System.out.println("remove");
+    public static void remove(int[] array, int index) {
 
-        int[] arr = new int[array3.length - 1];
-        for (int i : array3) {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
-        if (index > array3.length - 1) {
-            System.out.print("index chka");
-        } else {
-            for (int i = 0; i < array3.length - 1; i++) {
-                if (i < index) {
-                    arr[i] = array3[i];
-                } else {
-                    arr[i] = array3[i + 1];
-                }
-            }
-            for (int i : arr) {
-                System.out.print(i + ", ");
-            }
-        }
-        System.out.println();
-        System.out.println("_______________________");
     }
 
 
     /**
      * Calculate and return sum of array's elements.
      */
-    public static long sum(int[] array5) {
-        System.out.println("sum");
+    public static long sum(int[] array) {
 
-        int sum = 0;
-        for (int i : array5) {
-            sum += array5[i];
-        }
-        return sum;
+        return 0;
     }
 
 
     /**
      * Get the max value from the array.
      */
-    public static int getMax(int[] array6) {
-        System.out.println("getMax");
+    public static int getMax(int[] array) {
 
-        int max = array6[0];
-        for (int i : array6) {
-            if (max < array6[i]) {
-                max = array6[i];
-            }
-        }
-        return max;
+        return 0;
     }
 
     /**
      * Get the minimum value from the array
      */
-    public static void getMin(int[] array7) {
-        System.out.println("getMin");
+    public static void getMin(int[] array) {
 
-        int min = array7[0];
-        int j = min;
-        for (int i : array7) {
-            if (min > array7[i]) {
-                min = array7[i];
-            }
-        }
-        System.out.println(j);
     }
 
     /**
      * Calculate average of array
      */
-    public static double getAvg(int[] array8) {
-        System.out.println("getAvg");
+    public static int getAvg(int[] array) {
 
-        double sum = 0;
-        for (int i : array8) {
-            sum += array8[i];
-        }
-        return sum / array8.length;
+        return 0;
     }
+
 
 }
--- src/arrays/ArrayUtilTest.java
+++ src/arrays/ArrayUtilTest.java
@@ -1,47 +1,8 @@
 package arrays;
 
-import java.io.OutputStream;
-import java.util.Arrays;
-
-import static arrays.ArrayUtil.remove;
-
 public class ArrayUtilTest {
 
     public static void main(String[] args) {
-        ArrayUtil.createArray(5);
 
-        ArrayUtil.create2dArray(10, 8);
-
-        int[] array = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(Arrays.toString(ArrayUtil.addFirst(array, 15)));
-        System.out.println("_______________________");
-
-        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(ArrayUtil.contains(array1, 4));
-        System.out.println("_______________________");
-
-        int[] array2 = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(ArrayUtil.indexOf(array2, 6));
-        System.out.println("_______________________");
-
-        int[] array3 = {0, 1, 2, 3, 4, 5, 6, 7};
-        ArrayUtil.remove(array3, 4);
-
-
-        int[] array5 = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(ArrayUtil.sum(array5));
-        System.out.println("_______________________");
-
-        int[] array6 = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(ArrayUtil.getMax(array6));
-        System.out.println("_______________________");
-
-        int[] array7 = {0, 1, 2, 3, 4, 5, 6, 7};
-        ArrayUtil.getMin(array7);
-        System.out.println("_______________________");
-
-        int[] array8 = {0, 1, 2, 3, 4, 5, 6, 7};
-        System.out.println(ArrayUtil.getAvg(array8));
-        System.out.println("_______________________");
     }
 }
