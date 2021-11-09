4247781c8290fd48850045471f9a4c94106ffb6c
Vahann <yan.vahan@gmail.com>
2021-11-09 19:20:09 +0300

    Homework_5_Vahan_Harutyunyan_Stack_Math

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,56 +1,83 @@
 package stack;
 
+import java.util.Arrays;
+
 public class Stack {
 
-	static final int DEFAULT_SIZE = 16;
-	private int[] data;
-	private int tos; //index which determines the top of stack
-
-	public Stack() {
-		this(DEFAULT_SIZE);
-	}
-
-	public Stack(int size) {
-		data = new int[size];
-		this.tos = -1;
-	}
-
-	/**
-	 * Add element into stack
-	 */
-	public void push(int value) {
-
-	}
-
-	/**
-	 * Get element from the stack
-	 * @return
-	 */
-	public int pop() {
-
-		return 0;
-	}
+    static final int DEFAULT_SIZE = 16;
+    private int[] data;
+    private int tos; //index which determines the top of stack
 
 
-	/**
-	 * Clear stack
-	 */
-	public void clear() {
+    public Stack() {
+        this(DEFAULT_SIZE);
+    }
 
-	}
+    public Stack(int size) {
+        data = new int[size];
+        this.tos = -1;
+    }
 
-	/**
-	 * Check if stack is empty
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return false;
-	}
+    /**
+     * Add element into stack
+     */
+    public void push(int value) {
+        if (tos == data.length - 1) {
+            ensureCapacity();
+        }
+        data[++tos] = value;
+    }
 
-	/**
-	 * Increase the stack capacity if there is not enough space to add additional items
-	 */
-	private void ensureCapacity() {
+    /**
+     * Get element from the stack
+     *
+     * @return
+     */
+    public int pop() {
+        if (isEmpty()) {
+            System.out.print(" Stack is empty ");
+            return -1;
+        } else {
+            return data[tos--];
+        }
+    }
 
-	}
+
+    /**
+     * Clear stack
+     */
+    public void clear() {
+
+        data = new int[DEFAULT_SIZE];
+        tos = -1;
+
+    }
+
+    /**
+     * Check if stack is empty
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return 0 > tos;
+    }
+
+    /**
+     * Increase the stack capacity if there is not enough space to add additional items
+     */
+    private void ensureCapacity() {
+
+        int[] dataTemp = new int[data.length + 10];
+        System.arraycopy(data, 0, dataTemp, 0, data.length);
+        data = dataTemp;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "data=" + Arrays.toString(data) +
+                ", tos=" + tos +
+                '}';
+    }
 }
