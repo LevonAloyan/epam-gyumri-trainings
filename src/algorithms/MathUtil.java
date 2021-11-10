fc1b9c302b4dd72d60fa5d6288d39aefc61f6518
ZhenyaManukyan <zhenya-manukyan@inbox.ru>
2021-11-10 16:24:05 +0400

    Homework_5_MathUtil

--- src/algorithms/MathUtil.java
+++ src/algorithms/MathUtil.java
@@ -43,7 +43,7 @@ public class MathUtil {
     public static int factorial(int n) {
         int result = 1;
 
-        if(n == 1 ) return n;
+        if(n == 1 | n == 0) return 1;
 
         if(n < 0) result = factorial(n + 1) * n;
         if(n > 0) result = factorial(n - 1) * n;
