b1885323d8d6783fdc7d6d6b026e2679d1033b46
armenmelkonyan90 <armen.melkonyan90@gmail.com>
2021-11-11 16:42:28 +0400

    Done homework 5

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,56 +1,80 @@
 package stack;
 
+
+
 public class Stack {
+    static final int DEFAULT_SIZE = 16;
+    private int[] data;
+    private int tos; //index which determines the top of stack
 
-	static final int DEFAULT_SIZE = 16;
-	private int[] data;
-	private int tos; //index which determines the top of stack
+    public Stack() {
+        this(DEFAULT_SIZE);
+    }
 
-	public Stack() {
-		this(DEFAULT_SIZE);
-	}
+    public Stack(int size) {
+        data = new int[size];
+        this.tos = -1;
+    }
+    public void push(int value) {
+        if (tos == DEFAULT_SIZE) {
+            System.out.println("Stack is full ");
+        } else {
+            data[++tos] = value;
 
-	public Stack(int size) {
-		data = new int[size];
-		this.tos = -1;
-	}
+            System.out.print(data[value] + ",");
 
-	/**
-	 * Add element into stack
-	 */
-	public void push(int value) {
+        }
 
-	}
-
-	/**
-	 * Get element from the stack
-	 * @return
-	 */
-	public int pop() {
-
-		return 0;
-	}
+    }
 
 
-	/**
-	 * Clear stack
-	 */
-	public void clear() {
 
-	}
 
-	/**
-	 * Check if stack is empty
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return false;
-	}
+    public int pop() {
+        if (tos < 0) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else
+            return data[tos--];
 
-	/**
-	 * Increase the stack capacity if there is not enough space to add additional items
-	 */
-	private void ensureCapacity() {
 
-	}
+
+
+    }
+
+
+    public void clear(){
+        for(int i=0;i<DEFAULT_SIZE;i++){
+            data[i]=0;
+            System.out.print(data[i]+",");
+        }
+
+    }
+    public boolean isEmpty() {
+        if (tos < 0) {}
+            return false;
+        }
+        private void ensureCapacity() {
+            if (tos==DEFAULT_SIZE){
+                int[] newData= new int[DEFAULT_SIZE+1];
+                newData = data;
+
+            }
+        }
+
+
+
+    public static void main(String args[]){
+        Stack mystack = new Stack();
+        for(int i=0;i<DEFAULT_SIZE;i++) {
+            mystack.push(i);
+        }
+        System.out.println();
+        for(int i=0;i<DEFAULT_SIZE;i++){
+            System.out.print(mystack.pop()+" ");
+        }
+        System.out.println();
+        mystack.clear();
+        mystack.isEmpty();
+    }
 }
