ebe9779ebdc32ff49a7f0121f6b13aa840e4e0a2
Gurgen Atanesyan <atanesyangug@gmail.com>
2021-11-11 10:56:43 +0400

    Done Homework

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,55 +2,82 @@ package stack;
 
 public class Stack {
 
-	static final int DEFAULT_SIZE = 16;
-	private int[] data;
-	private int tos; //index which determines the top of stack
+    static final int DEFAULT_SIZE = 16;
+    private int[] data;
+    private int tos; //index which determines the top of stack
 
-	public Stack() {
-		this(DEFAULT_SIZE);
-	}
+    public Stack() {
+        this(DEFAULT_SIZE);
+    }
 
-	public Stack(int size) {
-		data = new int[size];
-		this.tos = -1;
-	}
+    public Stack(int size) {
+        data = new int[size];
+        this.tos = -1;
+    }
 
-	/**
-	 * Add element into stack
-	 */
-	public void push(int value) {
+    /**
+     * Add element into stack
+     */
+    public void push(int value) {
 
-	}
+        if (tos >= data.length-1) {
+            ensureCapacity();
 
-	/**
-	 * Get element from the stack
-	 * @return
-	 */
-	public int pop() {
+        }
+        tos++;
+        data[tos] = value;
+    }
 
-		return 0;
-	}
+    /**
+     * Get element from the stack
+     */
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("stack is empty");
+            return 0;
+        }
+        return (data[tos--]);
+    }
 
 
-	/**
-	 * Clear stack
-	 */
-	public void clear() {
+    /**
+     * Clear stack
+     */
+    public void clear() {
+        for (int i = 0; i <= tos; i++) {
+            data[i] = 0;
+        }
+        tos = -1;
+    }
 
-	}
+    /**
+     * Check if stack is empty
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        if (tos < 0) {
+            return true;
+        }
+        return false;
 
-	/**
-	 * Check if stack is empty
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return false;
-	}
+    }
 
-	/**
-	 * Increase the stack capacity if there is not enough space to add additional items
-	 */
-	private void ensureCapacity() {
+    /**
+     * Increase the stack capacity if there is not enough space to add additional items
+     */
+    private void ensureCapacity() {
+        int newData[] = new int[data.length * 2];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
 
-	}
+
+    public void print() {
+        for (int i = 0; i <= tos; i++) {
+            System.out.print(data[i] + ",");
+        }
+        System.out.println();
+    }
+
 }
--- src/stack/StackTest.java
+++ src/stack/StackTest.java
@@ -1,2 +1,26 @@
-package stack;public class StackTest {
+package stack;
+
+public class StackTest {
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        for (int i = 0; i < 54; i++) {
+            stack.push(i);
+
+        }
+        for (int i=0; i<50;i++){
+            System.out.print(stack.pop()+ ",");
+        }
+        System.out.println("---------------");
+        stack.push(41);
+        System.out.println("---------------");
+        stack.print();
+        System.out.println("---------------");
+        stack.clear();
+        stack.push(72);
+        System.out.println("---------------");
+        stack.print();
+
+
+    }
 }
