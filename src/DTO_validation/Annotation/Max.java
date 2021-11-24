d58daa55c531bebb7d1f00a4f84a807535b8a383
Gohar Minasyan <goharminasyan80@gmail.com>
2021-11-24 15:33:28 +0400

    Homework_7_annotation_exceptio_completely_finished

---
+++ src/DTO_validation/Annotation/Max.java
@@ -0,0 +1,10 @@
+package DTO_validation.Annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Max {
+    int value();
+    String maxErrorMessage();
+}
--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -1,5 +1,4 @@
 package algorithms;
-
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
