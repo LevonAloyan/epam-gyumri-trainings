dabc2f8ba4378258c59bedc8a2aeac34324ca1d4
Armine Zakaryan <ArmineZak>
2021-11-28 17:26:11 +0400

    Done homework 9.Changed Stack into generic type.

--- src/stackWithExceptions/Stack.java
+++ src/stackWithExceptions/Stack.java
@@ -1,17 +1,17 @@
 package stackWithExceptions;
 import java.util.Arrays;
 
-public class Stack  {
+public class Stack<T>  {
         public static void main(String[] args) {
-            Stack stack = new Stack(5);
+            Stack<String> stack = new Stack();
             System.out.println(stack);
-            stack.push(1);
-            stack.push(2);
-            stack.push(3);
-            stack.push(4);
-            stack.push(5);
-            stack.push(6);
-            stack.push(7);
+            stack.push("1");
+            stack.push("2");
+            stack.push("3");
+            stack.push("4");
+            stack.push("5");
+            stack.push("6");
+            stack.push("7");
             System.out.println(stack);
             stack.pop();
             System.out.println(stack);
@@ -19,12 +19,10 @@ public class Stack  {
             stack.clear();
             stack.pop();
             System.out.println(stack);
-
-
         }
 
         static final int DEFAULT_SIZE = 16;
-        private int[] data;
+        private T[] data;
         private int tos; //index which determines the top of stack
 
         public Stack() {
@@ -32,12 +30,12 @@ public class Stack  {
         }
 
         public Stack(int size) {
-            data = new int[size];
+            data =(T[]) new Object[size];
             this.tos = 0;
         }
 
 
-        public void push(int value) {
+        public void push(T value) {
             if (tos == data.length) {
                 throw new StackOverflowException("Stack is full, no more elements can be added.");
             } else {
@@ -46,7 +44,7 @@ public class Stack  {
         }
 
 
-        public int pop()  {
+        public T pop()  {
             if (isEmpty()) {
                 throw new EmptyStackException("Stack is empty, no more elements to pop.");
             } else {
@@ -60,7 +58,7 @@ public class Stack  {
                 System.out.println("Stack is empty.");
             } else {
                 for (int i = data.length - 1; i >= 0; i--) {
-                    data[i] = 0;
+                     data[i] = null;
                 }
             }
         }
@@ -72,7 +70,7 @@ public class Stack  {
 
         private void ensureCapacity() {
             if (tos == data.length) {
-                int[] newData = new int[3 * data.length/2];
+                T[] newData = (T[]) new Object[3 * data.length/2];
                 for (int i = 0; i < data.length; i++) {
                     newData[i] = data[i];
                 }
@@ -82,8 +80,8 @@ public class Stack  {
 
         @Override
         public String toString() {
-            int[] itmes = Arrays.copyOfRange(data, 0, tos);
-            return Arrays.toString(itmes);
+            T[] items =(T[]) Arrays.copyOfRange(data, 0, tos);
+            return Arrays.toString(items);
         }
 
     }
