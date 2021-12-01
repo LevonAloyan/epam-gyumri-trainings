c4fae26aac780f8c70d74dbe1acd7b15dfd73797
Gurgen Atanesyan <atanesyangug@gmail.com>
2021-12-01 14:27:19 +0400

    Generic for Stack

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,23 +1,23 @@
 package stack;
 
-public class Stack {
+public class Stack <T>{
 
     static final int DEFAULT_SIZE = 16;
-    private int[] data;
+    private T [] data;
     private int tos; //index which determines the top of stack
 
     public Stack() {
-        data = new int[DEFAULT_SIZE];    }
+        data = (T[]) new Object[DEFAULT_SIZE];    }
 
     public Stack(int size) {
-        data = new int[size];
+        data = (T[]) new Object[size];
         this.tos = -1;
     }
 
     /**
      * Add element into stack
      */
-    public void push(int value) {
+    public void push(T value) {
 
         if (tos >= data.length-1) {
             throw new StackOverFlowException("Stack is fully");
@@ -29,7 +29,7 @@ public class Stack {
     /**
      * Get element from the stack
      */
-    public int pop() {
+    public T pop() {
 
            if (isEmpty()){
                throw new EmptyStackException("Stack is empty");
@@ -44,9 +44,6 @@ public class Stack {
      * Clear stack
      */
     public void clear() {
-        for (int i = 0; i <= tos; i++) {
-            data[i] = 0;
-        }
         tos = -1;
     }
 
@@ -56,10 +53,7 @@ public class Stack {
      * @return
      */
     public boolean isEmpty() {
-        if (tos < 0) {
-            return true;
-        }
-        return false;
+        return tos < 0;
 
     }
 
@@ -67,7 +61,7 @@ public class Stack {
      * Increase the stack capacity if there is not enough space to add additional items
      */
     private void ensureCapacity() {
-        int newData[] = new int[data.length * 2];
+        T newData[] = (T[]) new Object[data.length * 2];
         System.arraycopy(data, 0, newData, 0, data.length);
         data = newData;
     }
--- src/stack/StackTest.java
+++ src/stack/StackTest.java
@@ -3,23 +3,24 @@ package stack;
 public class StackTest {
 
     public static void main(String[] args) {
-        Stack stack = new Stack(30);
-        for (int i = 0; i < 54; i++) {
-            stack.push(i);
-
-        }
-        for (int i=0; i<50;i++){
-            System.out.print(stack.pop()+ ",");
-        }
-        System.out.println("---------------");
-        stack.push(41);
-        System.out.println("---------------");
-        stack.print();
-        System.out.println("---------------");
-        stack.clear();
-        stack.push(72);
-        System.out.println("---------------");
-        stack.print();
+        Stack<Integer> stack = new Stack<>(30);
+        stack.push(59);
+//        for (int i = 0; i < 54; i++) {
+//            stack.push(i);
+//
+//        }
+//        for (int i=0; i<50;i++){
+//            System.out.print(stack.pop()+ ",");
+//        }
+//        System.out.println("---------------");
+//        stack.push(41);
+//        System.out.println("---------------");
+//        stack.print();
+//        System.out.println("---------------");
+//        stack.clear();
+//        stack.push(72);
+//        System.out.println("---------------");
+//        stack.print();
 
 
     }
