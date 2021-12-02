cf204fc6c68d1144d182e0e3f481ab1b9447442b
Levon_Aloyan <>
2021-12-02 22:28:52 +0400

    Adding IO homework

---
+++ src/io/FileUtil.java
@@ -0,0 +1,52 @@
+package io;
+
+import java.io.File;
+import java.util.List;
+
+public class FileUtil {
+
+
+    /**
+     * Implement a method that will find files matching the specified filename mask.
+     * Note that in the specified directory, in addition to the files themselves, there may also be other directories,
+     * you need to search for files in subdirectories as well. (sounds like a recursion).
+     *
+     * @param dirToSearchIn directory to search files
+     * @param fileNameMask file name mask to match files name
+     * @return list of files
+     */
+    public static List<File> search(File dirToSearchIn, String fileNameMask) {
+
+        return null;
+    }
+
+
+    /**
+     * Write into .txt file all possible combinations of phone numbers that start with your phone code
+     * for example, my phone code is 098. In the output file must be phone numbers starting from 098000000 to 098999999
+     */
+    public static void printPhoneNumbers(){
+
+    }
+
+    /**
+     * Serialize the object to a file, excluding the phone field and encrypt the bank card number
+     * (you can use Base64 https://www.baeldung.com/java-base64-encode-and-decode)
+     * @param user
+     * @param filePath
+     */
+    public static void serialize(User user, String filePath){
+
+    }
+
+    /**
+     * Deserialize the object from the file by decrypting the card number.
+     * @param filePath
+     * @return
+     */
+    public static User deserialize(String filePath){
+
+        return null;
+    }
+
+}
---
+++ src/io/User.java
@@ -0,0 +1,60 @@
+package io;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String password;
+    private String bankCardNumber;
+    private String phoneNumber;
+    private Address address;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBankCardNumber() {
+        return bankCardNumber;
+    }
+
+    public void setBankCardNumber(String bankCardNumber) {
+        this.bankCardNumber = bankCardNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
