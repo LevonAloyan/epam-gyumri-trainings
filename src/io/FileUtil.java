472267d0a3709e4ef28cd75e367800acda7fca45
davbalabekyan <balabekyandavo@gmail.com>
2021-12-03 23:46:57 +0400

    I write printPhoneNumbers method

--- src/io/FileUtil.java
+++ src/io/FileUtil.java
@@ -1,9 +1,15 @@
 package io;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 
 public class FileUtil {
+
+    public static void main(String[] args) {
+
+        printPhoneNumbers();
+
+    }
 
 
     /**
@@ -12,7 +18,7 @@ public class FileUtil {
      * you need to search for files in subdirectories as well. (sounds like a recursion).
      *
      * @param dirToSearchIn directory to search files
-     * @param fileNameMask file name mask to match files name
+     * @param fileNameMask  file name mask to match files name
      * @return list of files
      */
     public static List<File> search(File dirToSearchIn, String fileNameMask) {
@@ -25,26 +31,45 @@ public class FileUtil {
      * Write into .txt file all possible combinations of phone numbers that start with your phone code
      * for example, my phone code is 098. In the output file must be phone numbers starting from 098000000 to 098999999
      */
-    public static void printPhoneNumbers(){
-
+    public static void printPhoneNumbers() {
+        String fileName = "phoneNumbers.txt";
+        String message;
+        try {
+            OutputStream outputStream = new FileOutputStream(fileName);
+            for (int i = 93000000; i <= 93999999; i++) {
+                message = "0" + i + "\n";
+                byte[] bytes = message.getBytes();
+                outputStream.write(bytes);
+                if (i > 93999999) {
+                    outputStream.close();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.printf("File %s not found", fileName);
+        } catch (IOException e) {
+            System.out.println("Error during write");
+        }
     }
+
 
     /**
      * Serialize the object to a file, excluding the phone field and encrypt the bank card number
      * (you can use Base64 https://www.baeldung.com/java-base64-encode-and-decode)
+     *
      * @param user
      * @param filePath
      */
-    public static void serialize(User user, String filePath){
+    public static void serialize(User user, String filePath) {
 
     }
 
     /**
      * Deserialize the object from the file by decrypting the card number.
+     *
      * @param filePath
      * @return
      */
-    public static User deserialize(String filePath){
+    public static User deserialize(String filePath) {
 
         return null;
     }
