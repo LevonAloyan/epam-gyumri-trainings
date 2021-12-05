7f5a03f71163f761cd876dac66b67f7576366a8c
Armine Zakaryan <ArmineZak>
2021-12-05 21:54:55 +0400

    Modified printPhoneNumbers() method.

--- src/io/FileUtil.java
+++ src/io/FileUtil.java
@@ -10,11 +10,11 @@ public class FileUtil {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\VG\\epam-gyumri-trainings\\src");
         System.out.println(search(file, "user.bin"));
-        printPhoneNumbers();
         Address address = new Address("RA", "Gyumri", "3125", "105ave", "123");
         User user = new User(1, "AAA", "1", "0123456789123456", "099", address);
         serialize(user, "src\\io\\user.bin");
         deserialize("src\\io\\user.bin");
+       // printPhoneNumbers("091", "src\\io\\phoneNumbers1.txt");
     }
 
 
@@ -40,66 +40,66 @@ public class FileUtil {
                 search(file, fileNameMask);
             }
         }
-            return files;
+        return files;
     }
 
-        /**
-         * Write into .txt file all possible combinations of phone numbers that start with your phone code
-         * for example, my phone code is 098. In the output file must be phone numbers starting from 098000000 to 098999999
-         */
-        public static void printPhoneNumbers () {
-            String fileName = "phoneNumbers.txt";
-            String message;
-            try {
-                OutputStream outputStream = new FileOutputStream(fileName);
-                for (int i = 93000000; i <= 93999999; i++) {
-                    message = "0" + i + "\n";
-                    byte[] bytes = message.getBytes();
-                    outputStream.write(bytes);
-                    if (i > 93999999) {
-                        outputStream.close();
-                    }
+    /**
+     * Write into .txt file all possible combinations of phone numbers that start with your phone code
+     * for example, my phone code is 098. In the output file must be phone numbers starting from 098000000 to 098999999
+     */
+    public static void printPhoneNumbers(String phoneCode, String filePath) {
+
+        String message;
+        try {
+            OutputStream outputStream = new FileOutputStream(filePath);
+            for (int i = 1000000; i <= 1999999; i++) {
+                message = phoneCode + Integer.valueOf(i).toString().substring(1,7) + "\n";
+                byte[] bytes = message.getBytes();
+                outputStream.write(bytes);
+                if (i > 1999999) {
+                    outputStream.close();
                 }
-            } catch (FileNotFoundException e) {
-                System.out.printf("File %s not found", fileName);
-            } catch (IOException e) {
-                System.out.println("Error during write");
             }
+        } catch (FileNotFoundException e) {
+            System.out.printf("File %s not found", filePath);
+        } catch (IOException e) {
+            System.out.println("Error during write");
         }
-
-
-        /**
-         * Serialize the object to a file, excluding the phone field and encrypt the bank card number
-         * (you can use Base64 https://www.baeldung.com/java-base64-encode-and-decode)
-         *
-         * @param user
-         * @param filePath
-         */
-        public static void serialize (User user, String filePath){
-            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src\\io\\user.bin"))) {
-                oos.writeObject(user);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        /**
-         * Deserialize the object from the file by decrypting the card number.
-         *
-         * @param filePath
-         * @return
-         */
-        public static User deserialize (String filePath){
-            User user = null;
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src\\io\\user.bin"))) {
-                user = (User) ois.readObject();
-                System.out.println(user);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            return user;
-        }
-
     }
+
+
+    /**
+     * Serialize the object to a file, excluding the phone field and encrypt the bank card number
+     * (you can use Base64 https://www.baeldung.com/java-base64-encode-and-decode)
+     *
+     * @param user
+     * @param filePath
+     */
+    public static void serialize(User user, String filePath) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src\\io\\user.bin"))) {
+            oos.writeObject(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Deserialize the object from the file by decrypting the card number.
+     *
+     * @param filePath
+     * @return
+     */
+    public static User deserialize(String filePath) {
+        User user = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src\\io\\user.bin"))) {
+            user = (User) ois.readObject();
+            System.out.println(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+}
