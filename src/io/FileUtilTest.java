b8d3c1dedc0783576c69d132fe58192adfd2015b
AnjelaMelqonyan <anma2014melqonyan@gmail.com>
2021-12-06 14:28:30 +0400

    Done homework 10

--- src/io/FileUtilTest.java
+++ src/io/FileUtilTest.java
@@ -7,8 +7,8 @@ public class FileUtilTest {
         String pathSerialized="src/io/User.txt";
         String fileNameMask="src/io/";
         File dirToSearchIn=new File(fileNameMask);
-        FileUtil.printPhoneNumbers();
-        FileUtil.search(dirToSearchIn,fileNameMask);
+//        FileUtil.printPhoneNumbers();
+//        FileUtil.search(dirToSearchIn,fileNameMask);
         User user=new User();
         user.setId(5);
         user.setUsername("abc");
@@ -16,7 +16,11 @@ public class FileUtilTest {
         user.setPassword("adf");
         user.setBankCardNumber("92");
         user.setPhoneNumber("093");
-        FileUtil.serialize(user,pathSerialized);
+//        FileUtil.serialize(user,pathSerialized);
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
