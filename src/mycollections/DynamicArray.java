7eecdb1b50249b6e6857a1ece4083c4445b89226
Lilit Manukyan <lilitmanukyan888@gmail.com>
2021-12-20 01:17:31 +0400

    Done homework collection

--- src/mycollections/DynamicArray.java
+++ src/mycollections/DynamicArray.java
@@ -1,59 +1,159 @@
 package mycollections;
 
-public class DynamicArray<E>  implements MyList<E> {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-	@Override
-	public int size() {
-		return 0;
-	}
+public class DynamicArray<E> implements MyList<E> {
 
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
+    private static final int DEFAULT_SIZE_LIMIT = 10;
+    private E[] myArrayList;
+    private int maxSize;
 
-	@Override
-	public boolean contains(E element) {
-		return false;
-	}
+    public DynamicArray() {
+        this(DEFAULT_SIZE_LIMIT);
+    }
 
-	@Override
-	public int indexOf(E element) {
-		return 0;
-	}
+    public DynamicArray(int size) {
+        myArrayList = (E[]) new ArrayList[size];
+        maxSize = size;
+    }
 
-	@Override
-	public int lastIndexOf(E element) {
-		return 0;
-	}
 
-	@Override
-	public E get(int index) {
-		return null;
-	}
+    @Override
+    public int size() {
 
-	@Override
-	public E set(int index, E element) {
-		return null;
-	}
+        return maxSize;
+    }
 
-	@Override
-	public boolean add(E e) {
-		return false;
-	}
+    @Override
+    public boolean isEmpty() {
 
-	@Override
-	public void add(int index, E element) {
+        return size() == 0;
+    }
 
-	}
+    @Override
+    public boolean contains(E element) {
 
-	@Override
-	public E remove(int index) {
-		return null;
-	}
+        return indexOf(element) != -1;
+    }
 
-	@Override
-	public boolean remove(E element) {
-		return false;
-	}
+    @Override
+    public int indexOf(E element) {
+
+        if (element == null) {
+            for (int i = 0; i < maxSize; i++) {
+                if (myArrayList[i] == null) {
+                    return i;
+                }
+            }
+        }else {
+            for (int i = 0; i < maxSize; i++) {
+                if (element.equals(myArrayList[i])){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(E element) {
+        if (element == null) {
+            for (int i = maxSize-1; i > 0; i--) {
+                if (myArrayList[i] == null) {
+                    return i;
+                }
+            }
+        }else {
+            for (int i = maxSize-1; i > 0; i--) {
+                if (element.equals(myArrayList[i])){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public E get(int index) {
+        rangeCheck(index);
+        return (E) myArrayList[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+
+        rangeCheck(index);
+        E oldElement = myArrayList[index];
+        myArrayList[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public boolean add(E e) {
+
+        if (maxSize == myArrayList.length) {
+            myArrayList = ensureCapacity();
+        }
+        myArrayList[maxSize] = e;
+        maxSize++;
+        return true;
+    }
+
+
+    @Override
+    public void add(int index, E element) {
+        rangeCheck(index);
+        for (int i = maxSize - 1; i >= index; i--) {
+            myArrayList[i + 1] = myArrayList[i];
+        }
+        myArrayList[index] = element;
+        maxSize++;
+
+    }
+
+    @Override
+    public E remove(int index) {
+        rangeCheck(index);
+        E el = myArrayList[index];
+        for (int i = index; i < maxSize; i++) {
+            myArrayList[i] = myArrayList[i + 1];
+        }
+        myArrayList[--maxSize] = null;
+        return el;
+
+    }
+
+    @Override
+    public boolean remove(E element) {
+        for (int i = 0; i < maxSize; i++)
+        {
+            if (myArrayList[i].equals(element))
+            {
+                remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= maxSize) {
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + maxSize);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "DynamicArray{ array=" + Arrays.toString(myArrayList) +
+                ", arraySize=" + maxSize +'}';
+    }
+
+    private E[] ensureCapacity() {
+        E[] newArray = (E[]) new Object[myArrayList.length * 2];
+        System.arraycopy(myArrayList, 0, newArray, 0, myArrayList.length);
+        myArrayList = newArray;
+        return myArrayList;
+    }
 }
--- src/mycollections/DynamicArrayTest.java
+++ src/mycollections/DynamicArrayTest.java
@@ -3,52 +3,76 @@ package mycollections;
 
 public class DynamicArrayTest {
 
+    public static DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+
     public static void main(String[] args) {
+        sizeTest();
+        isEmptyTest();
+        addTest();
+        lastIndexOfTest();
+        removeByIndexTest();
+        remove();
+        getTest();
+        indexOfTest();
+        containsTest();
+        addByIndexTest();
+
+
 
     }
 
-    public void sizeTest() {
-        //TODO change body of implemented methods
+    public static void sizeTest() {
+        System.out.println("Array size is " + dynamicArray.size());
     }
 
-    public void isEmptyTest() {
-        //TODO: change body of implemented methods
+    public static  void isEmptyTest() {
+        System.out.println("Array is empty: " + dynamicArray.isEmpty());
     }
 
-    public void containsTest() {
-        //TODO: change body of implemented methods
+    public static  void containsTest() {
+        System.out.println("Array is contains of 15" + dynamicArray.contains(15));
     }
 
-    public void indexOfTest() {
-        //TODO: change body of implemented methods
+    public static void indexOfTest() {
+        System.out.println("The indexOf 15 is " + dynamicArray.indexOf(15));
     }
 
-    public void lastIndexOfTest() {
-        //TODO: change body of implemented methods
+    public static  void lastIndexOfTest() {
+        System.out.println("The indexOf 15 is " + dynamicArray.indexOf(15));
     }
 
-    public void getTest() {
-        //TODO: change body of implemented methods
+    public static void getTest() {
+        System.out.println("Тhe element position is: " + dynamicArray.get(16));
     }
 
-    public void set(int index, Integer element) {
-        //TODO: change body of implemented methods
+    public static void set(int index, Integer element) {
+        System.out.println(dynamicArray.set(4, 65));
     }
 
-    public void addTest() {
-        //TODO: change body of implemented methods
+    public static void addTest() {
+        System.out.println(dynamicArray.add(15));
+        System.out.println(dynamicArray.add(0));
+        System.out.println(dynamicArray.add(26));
+        System.out.println(dynamicArray.add(-10));
+        System.out.println(dynamicArray.add(16));
+        System.out.println(dynamicArray.add(15));
+        System.out.println(dynamicArray.add(24));
+        System.out.println(dynamicArray.add(60));
+        System.out.println(dynamicArray.add(8));
     }
 
-    public void addByIndexTest() {
-        //TODO: change body of implemented methods
+    public static void addByIndexTest() {
+       dynamicArray.add(2, 30);
+       dynamicArray.add(3, 300);
     }
 
-    public void removeByIndexTest() {
-        //TODO: change body of implemented methods
+    public static void removeByIndexTest() {
+        System.out.println(dynamicArray.remove(4));
     }
 
-    public void remove() {
-         //TODO: change body of implemented methods
+    public static void remove() {
+        System.out.println(dynamicArray.remove(5));
+
     }
 
 }
