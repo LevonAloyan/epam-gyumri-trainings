0b4a8ac21c62451322d15ea34bb37f6f5c994415
Hakob Diloyan <hdiloyan88@gmail.com>
2021-12-19 23:28:19 +0400

    Homework Collections

--- src/mycollections/DynamicArrayTest.java
+++ src/mycollections/DynamicArrayTest.java
@@ -1,54 +1,50 @@
 package mycollections;
 
-
 public class DynamicArrayTest {
 
     public static void main(String[] args) {
+        DynamicArray<String> arraylist = new DynamicArray<>();
+        arraylist.add("A");
+        arraylist.add("B");
+        arraylist.add("C");
+        arraylist.add("D");
+        arraylist.add("A");
+        System.out.println(arraylist.add("E"));
+        arraylist.print();
+        arraylist.add(2,"X");
+        arraylist.print();
+        System.out.println(arraylist.remove(4));
+        arraylist.print();
+        System.out.println(arraylist.remove("C"));
+        arraylist.print();
+        System.out.println(arraylist.isEmpty());
+        System.out.println(arraylist.size());
+        System.out.println(arraylist.indexOf("B"));
+        System.out.println(arraylist.lastIndexOf("A"));
+        System.out.println(arraylist.contains("E"));
+        System.out.println(arraylist.set(1,"I"));
+        arraylist.print();
 
+        DoubleLinkedList<String> linkedList = new DoubleLinkedList<>();
+        linkedList.add("K");
+        linkedList.add("M");
+        linkedList.add("L");
+        linkedList.add("O");
+        linkedList.add("K");
+        System.out.println(linkedList.add("S"));
+        linkedList.print();
+        linkedList.add(3,"Y");
+        linkedList.print();
+        System.out.println(linkedList.remove(2));
+        linkedList.print();
+        System.out.println(linkedList.remove("O"));
+        linkedList.print();
+        System.out.println(linkedList.isEmpty());
+        System.out.println(linkedList.size());
+        System.out.println(linkedList.indexOf("M"));
+        System.out.println(linkedList.lastIndexOf("K"));
+        System.out.println(linkedList.contains("M"));
+        System.out.println(linkedList.set(2,"Z"));
+        linkedList.print();
     }
-
-    public void sizeTest() {
-        //TODO change body of implemented methods
-    }
-
-    public void isEmptyTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void containsTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void indexOfTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void lastIndexOfTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void getTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void set(int index, Integer element) {
-        //TODO: change body of implemented methods
-    }
-
-    public void addTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void addByIndexTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void removeByIndexTest() {
-        //TODO: change body of implemented methods
-    }
-
-    public void remove() {
-         //TODO: change body of implemented methods
-    }
-
 }
