7168327a3ea38124ec1d78f26cf3e015f9565073
sevak.martirosyan <sevak.martirosyan@ukg.com>
2021-12-20 17:47:35 +0400

    Added linked list get,add implementation.

---
+++ src/mycollections/ListIndexOutOfBoundException.java
@@ -0,0 +1,8 @@
+package mycollections;
+
+public class ListIndexOutOfBoundException extends RuntimeException {
+
+    public ListIndexOutOfBoundException(String message) {
+        super(message);
+    }
+}
