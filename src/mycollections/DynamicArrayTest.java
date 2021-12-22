4bf0af7fb86a551dd82e6fc38589e45f1549e538
Gurgen Atanesyan <atanesyangug@gmail.com>
2021-12-22 16:17:36 +0400

    Created DoubleLinkedList

--- src/mycollections/DynamicArrayTest.java
+++ src/mycollections/DynamicArrayTest.java
@@ -1,22 +1,17 @@
 package mycollections;
 
 
-import java.util.ArrayList;
-
 public class DynamicArrayTest {
 
     public static void main(String[] args) {
-        DynamicArray<String> dynamicArray = new DynamicArray<>();
+        DynamicArray<String> dynamicArray = new DynamicArray<>(3);
         dynamicArray.add("Gugo");
         dynamicArray.add("Hayk");
-        dynamicArray.add("Hovo");
-
+        dynamicArray.add("Artur");
         System.out.println(dynamicArray.contains("Gugo"));
-        System.out.println(dynamicArray.contains("Gugo"));
-
         dynamicArray.print();
-        System.out.println(dynamicArray.indexOf("Gugo"));
-        System.out.println(dynamicArray.lastIndexOf("Gugo"));
+        System.out.println(dynamicArray.indexOf("Hovo"));
+        System.out.println(dynamicArray.lastIndexOf("Hovo"));
         System.out.println(dynamicArray.get(2));
         dynamicArray.set(2,"Garik");
         dynamicArray.print();
@@ -27,7 +22,21 @@ public class DynamicArrayTest {
         dynamicArray.print();
         dynamicArray.remove(0);
         dynamicArray.print();
+
+        DoubleLinkedList<Integer > doubleLinkedList = new DoubleLinkedList<>();
+        doubleLinkedList.add(10);
+        doubleLinkedList.add(15);
+        doubleLinkedList.add(20);
+        doubleLinkedList.add(25);
+        doubleLinkedList.add(30);
+        System.out.println(doubleLinkedList.size());
+        System.out.println(doubleLinkedList.isEmpty());
+        System.out.println(doubleLinkedList.contains(10));
+        System.out.println(doubleLinkedList.indexOf(15));
+        System.out.println(doubleLinkedList.lastIndexOf(20));
     }
+
+
 
     public void sizeTest() {
         //TODO change body of implemented methods
@@ -74,3 +83,19 @@ public class DynamicArrayTest {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
