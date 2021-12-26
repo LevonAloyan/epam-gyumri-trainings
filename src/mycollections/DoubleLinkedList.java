8cb250dc0cbc64ad6d00757fcedd207eedfb702d
Garik <gtepanosyan@mail.ru>
2021-12-26 15:22:52 +0400

    I create custom LinkedList

--- src/mycollections/DoubleLinkedList.java
+++ src/mycollections/DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package mycollections;
 
+import java.util.Arrays;
+
 public class DoubleLinkedList<T> implements MyList<T> {
     private Node<T> head;
     private Node<T> last;
@@ -10,6 +12,39 @@ public class DoubleLinkedList<T> implements MyList<T> {
         this.head = null;
         this.last = null;
         this.size = 0;
+    }
+
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer> myLinkedList = new DoubleLinkedList<>();
+        int contains = 5;
+        int index = 2;
+        int remove = 3;
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+        myLinkedList.add(5);
+        System.out.println("Size: " + myLinkedList.size());
+        System.out.println("Remove: " + remove + "  " + myLinkedList.remove(remove));
+        System.out.println("Size: remove " + myLinkedList.size());
+        System.out.println(myLinkedList);
+        System.out.println("Contains number: " + contains + " " + myLinkedList.contains(contains));
+        System.out.println("Get Index: " + index + "   " + myLinkedList.get(index));
+        System.out.println("IndexOf method First element: " + myLinkedList.indexOf(5));
+        System.out.println("LastIndexOf method Last element: " + myLinkedList.lastIndexOf(5));
+
+
+    }
+
+    @Override
+    public String toString() {
+        Object[] result = new Object[size];
+        int i = 0;
+        for (DoubleLinkedList.Node<T> x = head; x != null; x = x.next)
+            result[i++] = x.data;
+        return Arrays.toString(result);
     }
 
     @Override
@@ -24,17 +59,53 @@ public class DoubleLinkedList<T> implements MyList<T> {
 
     @Override
     public boolean contains(T o) {
+        Node<T> nodeToReturn = head;
+        for (int i = 0; i < size; i++) {
+            if (nodeToReturn != null) {
+                if (!(nodeToReturn.getData().equals(o))) {
+                    nodeToReturn = nodeToReturn.getNext();
+                } else {
+                    return true;
+
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public int indexOf(T o) {
         // todo iterate on Linked list check if the data is equals to given value, return the index
+        Node<T> nodeToReturn = head;
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (nodeToReturn != null) {
+                if (!(nodeToReturn.getData().equals(o))) {
+                    nodeToReturn = nodeToReturn.getNext();
+                    index++;
+                } else {
+                    return index;
+                }
+            }
+
+        }
         return 0;
     }
 
     @Override
     public int lastIndexOf(T o) {
+        Node<T> nodeToReturn = last;
+
+        for (int i = size - 1; i >= 0; i--) {
+
+            if (nodeToReturn != null) {
+                if (!(nodeToReturn.getData().equals(o))) {
+                    nodeToReturn = nodeToReturn.getPrevious();
+                } else {
+                    return i;
+                }
+            }
+        }
         return 0;
     }
 
@@ -54,7 +125,18 @@ public class DoubleLinkedList<T> implements MyList<T> {
 
     @Override
     public T set(int index, T element) {
-        return null;
+        if (index > size || index < 0) {
+            throw new ListIndexOutOfBoundException("Index " + index + " is out of bound. The LinkedList size is " + size);
+        }
+        Node<T> nodeToReturn = head;
+        int index1 = 0;
+        while (index1 < index) {
+            nodeToReturn = nodeToReturn.getNext();
+            index1++;
+        }
+        T data = nodeToReturn.data;
+        nodeToReturn.data = element;
+        return data;
     }
 
     @Override
@@ -74,21 +156,93 @@ public class DoubleLinkedList<T> implements MyList<T> {
 
     @Override
     public void add(int index, T element) {
+        Node<T> nodeToReturn = head;
+        if (index >= 0 && index < size) {
+            method:
+            for (int i = 0; i < size; i++) {
+                if (nodeToReturn != null) {
+                    if (index == i) {
+                        Node<T> current = new Node<>(element);
+                        if (nodeToReturn.getPrevious() != null) {
+                            current.setNext(nodeToReturn);
+                            current.setPrevious(nodeToReturn.getPrevious());
+                            nodeToReturn.getPrevious().setNext(current);
+                            nodeToReturn.setPrevious(current);
+                        } else {
+                            head = current;
+                            current.setNext(nodeToReturn);
+                            nodeToReturn.setPrevious(current);
+                        }
+                        size++;
+                    } else nodeToReturn = nodeToReturn.getNext();
+                    continue method;
+                }
 
+            }
+        } else {
+            throw new ListIndexOutOfBoundException("\"Index \" + index + \" is out of bound. The LinkedList size is \" + size");
+        }
     }
 
     @Override
-    public T remove(int index) {
-        return null;
+    public T removeByIndex(int index) {
+        Node<T> nodeToReturn = head;
+        if (index >= 0 && index < size) {
+            method:
+            for (int i = 0; i < size; i++) {
+                if (index == i) {
+                    if (nodeToReturn != null) {
+                        if (nodeToReturn.getNext() != null && nodeToReturn.getPrevious() != null) {
+                            nodeToReturn.getPrevious().setNext(nodeToReturn.getNext());
+                            nodeToReturn.getNext().setPrevious(nodeToReturn.getPrevious());
+                            size--;
+                            return (T) nodeToReturn;
+                        } else if (nodeToReturn.getPrevious() == null) {
+                            nodeToReturn.getNext().setPrevious(null);
+                            head = nodeToReturn.getNext();
+                            nodeToReturn.setNext(null);
+                            size--;
+                            return (T) nodeToReturn;
+
+                        } else {
+                            nodeToReturn.getPrevious().setNext(null);
+                            last = nodeToReturn.getPrevious();
+                            nodeToReturn.setPrevious(null);
+                            size--;
+                            return (T) nodeToReturn;
+                        }
+                    }
+                } else {
+                    nodeToReturn = nodeToReturn.getNext();
+                    continue method;
+                }
+            }
+        }
+        return (T) nodeToReturn;
     }
 
     @Override
     public boolean remove(T o) {
+        Node<T> nodeToReturn = head;
+        for (int i = 0; i < size; i++) {
+            if (nodeToReturn != null) {
+                if (nodeToReturn.getData().equals(o)) {
+                    removeByIndex(indexOf(o));
+                    return true;
+                } else nodeToReturn = nodeToReturn.getNext();
+            }
+        }
         return false;
     }
 
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(get(i));
+        }
+    }
+
     private static class Node<T> {
-        private final T data;
+        private T data;
         private Node<T> next;
         private Node<T> previous;
 
@@ -104,32 +258,17 @@ public class DoubleLinkedList<T> implements MyList<T> {
             return next;
         }
 
-        public Node<T> getPrevious() {
-            return previous;
-        }
-
         public void setNext(Node<T> next) {
             this.next = next;
+        }
+
+        public Node<T> getPrevious() {
+            return previous;
         }
 
         public void setPrevious(Node<T> previous) {
             this.previous = previous;
         }
-    }
-
-    public static void main(String[] args) {
-        DoubleLinkedList<Integer> myLinkedList = new DoubleLinkedList<>();
-
-        myLinkedList.add(1);
-        myLinkedList.add(2);
-        myLinkedList.add(3);
-        myLinkedList.add(4);
-        myLinkedList.add(5);
-        myLinkedList.add(5);
-
-        System.out.println(myLinkedList.get(16));
-
-
     }
 }
 
