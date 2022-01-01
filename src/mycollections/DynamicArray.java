d5c811ef79ef1c2b744d030fe947ea8c81c787bb
Armine Zakaryan <ArmineZak>
2022-01-01 14:51:59 +0400

    Done homework 12_dynamicArray.

--- src/mycollections/DynamicArray.java
+++ src/mycollections/DynamicArray.java
@@ -1,59 +1,143 @@
 package mycollections;
 
-public class DynamicArray<E>  implements MyList<E> {
+public class DynamicArray<E> implements MyList<E> {
+    private final int INITIAL_CAPACITY = 16;
+    private E[] array;
+    private int size;
 
-	@Override
-	public int size() {
-		return 0;
-	}
 
-	@Override
-	public boolean isEmpty() {
-		return false;
-	}
+    public DynamicArray() {
+        this.array = (E[]) new Object[INITIAL_CAPACITY];
+    }
 
-	@Override
-	public boolean contains(E element) {
-		return false;
-	}
+    public DynamicArray(int length) {
+        this.array = (E[]) new Object[length];
+    }
 
-	@Override
-	public int indexOf(E element) {
-		return 0;
-	}
+    @Override
+    public int size() {
+        return this.size;
+    }
 
-	@Override
-	public int lastIndexOf(E element) {
-		return 0;
-	}
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
 
-	@Override
-	public E get(int index) {
-		return null;
-	}
+    @Override
+    public boolean contains(E element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	@Override
-	public E set(int index, E element) {
-		return null;
-	}
+    @Override
+    public int indexOf(E element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	@Override
-	public boolean add(E e) {
-		return false;
-	}
+    @Override
+    public int lastIndexOf(E element) {
+        for (int i = size; i > 0; i--) {
+            if (array[i] != null && array[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	@Override
-	public void add(int index, E element) {
+    @Override
+    public E get(int index) {
+        if (index > 0 || index <= size) {
+            for (int i = 0; i < size; i++) {
+                if (i == index) {
+                    return array[i];
+                }
+            }
+        }
+        return null;
+    }
 
-	}
+    @Override
+    public E set(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        } else {
+            return array[index] = element;
+        }
+    }
 
-	@Override
-	public E remove(int index) {
-		return null;
-	}
+    @Override
+    public boolean add(E e) {
+        array[size++] = e;
+        return true;
+    }
 
-	@Override
-	public boolean remove(E element) {
-		return false;
-	}
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException();
+        }
+        resize();
+        for (int i = size; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = element;
+        size++;
+    }
+
+    private void resize() {
+        if (array.length == size) {
+            E[] newArray = (E[]) new Object[size * 2];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+
+    @Override
+    public E remove(int index) {
+        if (index >= 0 || index <= size) {
+            for (int i = 0; i < size; i++) {
+                if (i == index) {
+                    E value = array[i];
+                    for (int j = i; j < size; j++) {
+                        array[j] = array[j + 1];
+                    }
+                    size--;
+                    return value;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                for (int j = i; j < size; j++) {
+                    array[j] = array[j + 1];
+                    size--;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
--- src/mycollections/Main.java
+++ src/mycollections/Main.java
@@ -1,24 +1,43 @@
 package mycollections;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.add(30);
-        list.add(40);
-        list.add(50);
-        list.add(30);
-
+//        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.add(30);
+//        list.add(40);
+//        list.add(50);
+//        list.add(30);
 //        System.out.println(list.contains(40));
 //        System.out.println(list.remove(Integer.valueOf(40)));
 //        list.add(3, 2);
 //        list.print(list);
 //        list.remove(0);
 //        list.print(list);
-        System.out.println(list.lastIndexOf(30));
-        list.set(4, 100);
-        list.print(list);
-   }
+//        System.out.println(list.lastIndexOf(30));
+//        list.set(4, 100);
+//        list.print(list);
+
+        DynamicArray<Integer> array = new DynamicArray<>();
+        array.add(10);
+        array.add(30);
+        array.add(20);
+        array.add(30);
+//        System.out.println(array.size());
+//        System.out.println(array.isEmpty());
+//        System.out.println(array.contains(10));
+//        System.out.println(array.indexOf(30));
+//        System.out.println(array.lastIndexOf(30));
+//        System.out.println(array.get(4));
+//        System.out.println(array.set(3, 50));
+//        array.add(4, 9);
+        System.out.println(array.add(9));
+        System.out.println(array.remove(Integer.valueOf(30)));
+        System.out.println(array.remove(4));
+        array.print();
+    }
 }
