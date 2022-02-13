357a6023fd73c4c302356235124f4f64551c6bc3
Hakob Diloyan <hdiloyan88@gmail.com>
2022-02-13 14:17:15 +0400

    The table employees has beeb changed

--- src/sql/homework.sql
+++ src/sql/homework.sql
@@ -14,21 +14,6 @@ CREATE TABLE `employees`.`departments` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `position_name_UNIQUE` (`position_name`));
 
-  CREATE TABLE `employees`.`departments_positions` (
-  `dept_id` INT NOT NULL,
-  `position_id` INT NOT NULL,
-  INDEX `dept_fk_idx` (`dept_id`),
-  INDEX `postition_fk_idx` (`position_id`),
-  CONSTRAINT `dept_fk`
-    FOREIGN KEY (`dept_id`)
-    REFERENCES `employees`.`departments` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `postition_fk`
-    FOREIGN KEY (`position_id`)
-    REFERENCES `employees`.`positions` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
 
 CREATE TABLE `employees`.`department_phones` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -36,8 +21,8 @@ CREATE TABLE `employees`.`department_phones` (
   `dept_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `phone_number_UNIQUE` (`phone_number`),
-  INDEX `dept_name_key_idx` (`dept_id`),
-  CONSTRAINT `dept_name_key`
+  INDEX `phone_dept_fk` (`dept_id`),
+  CONSTRAINT `phone_dept_fk`
     FOREIGN KEY (`dept_id`)
     REFERENCES `employees`.`departments` (`id`)
     ON DELETE CASCADE
@@ -49,12 +34,19 @@ CREATE TABLE `employees`.`employees` (
   `middle_name` VARCHAR(45) NOT NULL,
   `surname` VARCHAR(45) NOT NULL,
   `position_id` INT NOT NULL,
+  `department_id` INT NOT NULL,
   `corporate_car_is_available` BOOLEAN NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `employee_position_fk_idx` (`position_id`),
-  CONSTRAINT `employee_position_fk_idx`
+  INDEX `employee_position_fk` (`position_id`),
+  INDEX `employee_department_fk` (`department_id`),
+  CONSTRAINT `employee_position_fk`
     FOREIGN KEY (`position_id`)
     REFERENCES `employees`.`positions` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `employee_department_fk`
+    FOREIGN KEY (`department_id`)
+    REFERENCES `employees`.`departments` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
 
@@ -64,9 +56,10 @@ CREATE TABLE `employees`.`employee's_children` (
   `birth_date` DATE NOT NULL,
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `child_employee_fk_idx` (`employee_id`),
+  INDEX `child_employee_fk` (`employee_id`),
   CONSTRAINT `child_employee_fk`
     FOREIGN KEY (`employee_id`)
     REFERENCES `employees`.`employees` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+
