dffabe8aa715ee444ec6d9c5e27b8f54690790c1
Garik <gtepanosyan@mail.ru>
2022-02-14 15:23:10 +0400

    MySQL Databases

---
+++ src/homework.sql
@@ -0,0 +1,9 @@
+use epam_learn;
+show tables;
+select * from authors;
+select * from books;
+select * from books_authors;
+select * from books_genres;
+select * from genres;
+select * from subscribers;
+select * from subscriptions;
